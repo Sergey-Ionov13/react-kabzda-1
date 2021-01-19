256fb6c63de7f3dc5c81ff890b100b411137a148
Sergey-Ionov13 <s.dyadya13@gmail.com>
2021-01-19 21:50:49 +0300

    lesson 52 Side effect (User component)

--- src/Components/Users/UsersClass.jsx
+++ src/Components/Users/UsersClass.jsx
@@ -5,19 +5,16 @@ import us from './Users.module.css';
 
 const Users = (props) => {
 
-    let getUsers = () => {
-        if (props.users.length === 0) {
-            axios.get('https://social-network.samuraijs.com/api/1.0/users').then( response => {
-                props.setUsers(response.data.items);
-            } )
-        }
+    if (props.users.length === 0) {
+        axios.get('https://social-network.samuraijs.com/api/1.0/users').then( response => {
+            props.setUsers(response.data.items);
+        } )
     }
 
     return <div>
-        <button onClick={getUsers}>get users</button>
         {
             props.users.map(u =>
-                <div key={u.id}>
+                <div key={props.id}>
                     <div>
                         <img alt={''} src={u.photos.small !== null ? u.photos.small : userAva} className={us.ava}/>
                         <button onClick={() => {
@@ -38,6 +35,26 @@ const Users = (props) => {
             )
         }
     </div>
+    /*(
+        props.users.map( u =>
+            <div>
+                <div>
+                    <img alt={''} src={u.avaURL}/>
+                    <button onClick={props.followToggle}>{u.followed ? 'Unfollow' : 'Follow'}</button>
+                </div>
+                <div>
+                    <div>
+                        <span>{`${u.firstName} ${u.lastName}`}</span>
+                        <span>{u.status}</span>
+                    </div>
+                    <div>
+                        <span>{`${u.location.country},`}</span>
+                        <span>{u.location.city}</span>
+                    </div>
+                </div>
+            </div>
+        )
+    );*/
 }
 
 export default Users;
