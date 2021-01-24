f17a6b1d8bd2ad115f9029a810bf4d421b4811a2
Sergey-Ionov13 <s.dyadya13@gmail.com>
2021-01-24 21:05:01 +0300

    56 lesson Refactor Users & UsersContainer components

--- src/Components/Users/UsersContainer.jsx
+++ src/Components/Users/UsersContainer.jsx
@@ -1,13 +1,45 @@
 import {connect} from "react-redux";
-import Users from "./Users";
 import {
     followToggleAC,
     setCurrentPageAC,
     setTotalUsersCountAC,
     setUsersAC
 } from "../../Redux/user-reducer";
+import React from "react";
+import * as axios from "axios";
+import Users from "./Users";
 
-const mapStateToProps = (state) => {
+class UsersContainer extends React.Component {
+
+    componentDidMount() {
+        if (this.props.users.length === 0) {
+            axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageCapacity}`).then( response => {
+                this.props.setUsers(response.data.items);
+                this.props.setTotalUsersCount(response.data.totalCount);
+            } )
+        }
+    }
+
+    setCurrentPage = (pageNumber) => {
+        this.props.setCurrentPage(pageNumber);
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageCapacity}`).then( response => {
+            this.props.setUsers(response.data.items);
+            this.props.setTotalUsersCount(response.data.totalCount);
+        } )
+    }
+
+    render() {
+        return <Users totalUsersCount={this.props.totalUsersCount}
+                      pageCapacity={this.props.pageCapacity}
+                      currentPage={this.props.currentPage}
+                      setCurrentPage={this.setCurrentPage}
+                      users={this.props.users}
+                      followToggle={this.props.followToggle}
+        />
+    }
+}
+
+let mapStateToProps = (state) => {
     return {
         users: state.usersPage.users,
         totalUsersCount: state.usersPage.totalUsersCount,
@@ -16,6 +48,7 @@ const mapStateToProps = (state) => {
 
     }
 }
+
 const mapDispatchToProps = (dispatch) => {
     return {
         followToggle: (userID) => {
@@ -33,6 +66,4 @@ const mapDispatchToProps = (dispatch) => {
     }
 }
 
-const MyPostsContainer = connect(mapStateToProps, mapDispatchToProps)(Users);
-
-export default MyPostsContainer;
+export default connect(mapStateToProps, mapDispatchToProps)(UsersContainer);
